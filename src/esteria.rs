647d6bd15672a112ea320a185adf6018f9bb89f4
Olegs Korsaks <olegs@metisjean.com>
2026-01-16 20:28:41 +0200

    Fixed unit tests

--- src/esteria.rs
+++ src/esteria.rs
@@ -328,7 +328,7 @@ mod tests {
         let err = client.send_sms(base_request()).await.unwrap_err();
         match err {
             SmsError::SendFailed { number, message } => {
-                assert_eq!(number, "+12345678S90");
+                assert_eq!(number, "+1234567890");
                 assert_eq!(message, "unable to authenticate");
             }
             SmsError::RequestFailed(err) => panic!("Unexpected error type: {err}"),
