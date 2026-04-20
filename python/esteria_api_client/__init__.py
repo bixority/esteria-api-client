2e4f41d44a63870ba2513e6b2855ab5bbbdb70cc
Olegs Korsaks <olegs@metisjean.com>
2026-04-20 15:40:32 +0300

    Improved python typings

--- python/esteria_api_client/__init__.py
+++ python/esteria_api_client/__init__.py
@@ -1,3 +1,5 @@
-from ._esteria_api_client import PyEncoding, PySmsClient, PySmsFlags
+from ._esteria_api_client import PyEncoding as Encoding
+from ._esteria_api_client import PySmsClient as SmsClient
+from ._esteria_api_client import PySmsFlags as SmsFlags
 
-__all__ = ["PySmsClient", "PySmsFlags", "PyEncoding"]
+__all__ = ["SmsClient", "SmsFlags", "Encoding"]
